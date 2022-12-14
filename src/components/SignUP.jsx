930bfe3f77e9e5c2c00566ca2c52520ace901890
Akash_96 <realakash9630@gmail.com>
2022-12-14 14:31:37 +0530

    alter

--- src/components/SignUP.jsx
+++ src/components/SignUP.jsx
@@ -1,52 +1,41 @@
+import { Link } from "react-router-dom";
+
 function SignUp() {
   return (
     <div className="page">
       <main className="page__main">
         <form className="form page__form" action="">
           <div className="form__header">
-            <h1 className="form__title">Signup</h1>
+            <h1 className="text-center form_title">Signup</h1>
           </div>
+          {/* <div className="text-center">
+            Already registered?
+            <Link className="link" to="/">
+              {" "}
+              <strong>Log In</strong>
+            </Link>
+          </div> */}
           <div className="form__body">
-            <div className="form__linput">
-              <label className="form__label" htmlFor="uname">
-                <i className="fa fa-user" aria-hidden="true"></i>
-              </label>
-              <input
-                className="form__input"
-                type="text"
-                id="uname"
-                name="uname"
-                placeholder="Username"
-                required="required"
-              />
-            </div>
-            <div className="form__linput">
-              <label className="form__label" htmlFor="email">
-                <i className="fa fa-envelope form__icon"></i>
-              </label>
-              <input
-                className="form__input"
-                type="text"
-                id="email"
-                name="email"
-                placeholder="Email"
-                required="required"
-              />
-            </div>
-            <div className="form__linput">
-              <label className="form__label" htmlFor="psw">
-                <i className="fa fa-key" aria-hidden="true"></i>
-              </label>
-              <input
-                className="form__input"
-                type="password"
-                id="psw"
-                name="psw"
-                placeholder="Password"
-                required="required"
-              />
-            </div>
-            <input className="primary-button" type="submit" value="Signup" />
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="Username"
+            />
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+            />
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+            />
+            <input
+              type="submit"
+              className="btn btn-primary btn-block mb-4"
+              value="Submit"
+            />
           </div>
         </form>
       </main>
